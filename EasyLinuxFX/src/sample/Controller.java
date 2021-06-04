44d8f88ecc96e99c9d61feeaa392bc0f9559db12
pmoeini1 <pmoeini@uwo.ca>
2021-06-03 20:51:43 -0400

    Add methods

--- EasyLinuxFX/src/sample/Controller.java
+++ EasyLinuxFX/src/sample/Controller.java
@@ -1,4 +1,59 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.io.File;
+
 public class Controller {
+    @FXML
+    private TextField text;
+    @FXML
+    private Label out;
+
+    // show present working directory
+    public void presentDirectory() {
+        out.setText("Present Working Directory: " + System.getProperty("user.dir"));
+    }
+    // change current directory completely
+    public void currentDirectory(){
+        System.setProperty("user.dir", text.getText());
+        out.setText("Present Working Directory: " + System.getProperty("user.dir"));
+    }
+    // change to a path in current folder
+    public void currentInFolder() {
+        String path = System.getProperty("user.dir") + "\\" + text.getText();
+        System.setProperty("user.dir", path);
+        out.setText("Present Working Directory: " + System.getProperty("user.dir"));
+    }
+    // make folder
+    public void makeDir() {
+        File f = new File(System.getProperty("user.dir"));
+        if (f.mkdir()) {
+            out.setText("Folder created");
+        } else {
+            out.setText("Folder creation failed");
+        }
+    }
+
+    // list all paths "below" current folder
+    public void list() {
+        File f = new File(System.getProperty("user.dir"));
+        String[] list = f.list();
+        StringBuilder sb = new StringBuilder();
+        if (list.length < 1) {
+            out.setText("Empty folder");
+            return;
+        }
+        for (int i = 0; i < list.length; i++) {
+            sb.append("\n" + list[i]);
+            if (i > 9) {
+                sb.append("\n ...");
+                break;
+            }
+        }
+        out.setText(sb.toString());
+    }
+
 }
--- EasyLinuxFX/src/sample/Main.java
+++ EasyLinuxFX/src/sample/Main.java
@@ -8,12 +8,12 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
+    // show window and add icon image
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setTitle("Command Line GUI");
+        primaryStage.setScene(new Scene(root, 450, 400));
         primaryStage.show();
         primaryStage.getIcons().add(new Image("/sample/codepic.jpg"));
     }
