06237ca5c22c1ce3434527c754dfa0a30e55583f
pmoeini1 <pmoeini@uwo.ca>
2021-06-03 20:29:04 -0400

    Add photo

--- EasyLinuxFX/src/sample/Main.java
+++ EasyLinuxFX/src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,8 +13,9 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
+        primaryStage.getIcons().add(new Image("/sample/codepic.jpg"));
     }
 
 
